be8c285e03c270e137a89bad586bebad000c88a1
linknk3 <linh.nguyen3@finhay.com.vn>
2024-03-05 08:48:19 +0700

    fix Ãissues input field

--- src/main/java/vnscbyfinhay/api/auth/BaseAuth.java
+++ src/main/java/vnscbyfinhay/api/auth/BaseAuth.java
@@ -12,45 +12,45 @@ import static io.restassured.RestAssured.given;
 
 public class BaseAuth {
 
-    public static void verifyGet(String url, String token, Map<String, Object> params, int statusCode,String message, String errorCode) throws JsonProcessingException {
-        verify(RestMethod.GET, url, token, params, new HashMap<>(), new HashMap<>(), statusCode, message,errorCode);
+    public static void verifyGet(String url, String token, Map<String, Object> params, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
+        verify(RestMethod.GET, url, token, params, new HashMap<>(), new HashMap<>(), statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyGet(String url, String token, int statusCode,String message, String errorCode) throws JsonProcessingException {
-        verify(RestMethod.GET, url, token,new HashMap<>(),new HashMap<>(),new HashMap<>(), statusCode, message,errorCode);
+    public static void verifyGet(String url, String token, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
+        verify(RestMethod.GET, url, token,new HashMap<>(),new HashMap<>(),new HashMap<>(), statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyPost(String url, String token, Map<String, Object> body, int statusCode,String message, String errorCode) throws JsonProcessingException {
+    public static void verifyPost(String url, String token, Map<String, Object> body, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        verify(RestMethod.POST, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+        verify(RestMethod.POST, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyPost(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String message, String errorCode) throws JsonProcessingException {
-        verify(RestMethod.POST, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+    public static void verifyPost(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
+        verify(RestMethod.POST, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyPut(String url, String token, Map<String, Object> body, int statusCode,String message, String errorCode) throws JsonProcessingException {
+    public static void verifyPut(String url, String token, Map<String, Object> body, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        verify(RestMethod.PUT, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+        verify(RestMethod.PUT, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyPut(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String message, String errorCode) throws JsonProcessingException {
-        verify(RestMethod.PUT, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+    public static void verifyPut(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
+        verify(RestMethod.PUT, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyDelete(String url, String token, Map<String, Object> body, int statusCode,String message, String errorCode) throws JsonProcessingException {
+    public static void verifyDelete(String url, String token, Map<String, Object> body, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        verify(RestMethod.DELETE, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+        verify(RestMethod.DELETE, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verifyDelete(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String message, String errorCode) throws JsonProcessingException {
-        verify(RestMethod.DELETE, url, token, new HashMap<>(), body, headers, statusCode, message,errorCode);
+    public static void verifyDelete(String url, String token, Map<String, Object> body, Map<String, String> headers, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
+        verify(RestMethod.DELETE, url, token, new HashMap<>(), body, headers, statusCode, expectedMessage,expectdErrorCode);
     }
 
-    public static void verify(RestMethod method, String url, String token, Map<String, Object> params, Map<String, Object> body, Map<String, String> headers, int statusCode,String message, String errorCode) throws JsonProcessingException {
+    public static void verify(RestMethod method, String url, String token, Map<String, Object> params, Map<String, Object> body, Map<String, String> headers, int statusCode,String expectedMessage, String expectdErrorCode) throws JsonProcessingException {
         com.fasterxml.jackson.databind.ObjectMapper objectMapper = new ObjectMapper();
         String bodyString = objectMapper.writeValueAsString(body);
 
--- src/test/java/auth/brokers/TestAuth.java
+++ src/test/java/auth/brokers/TestAuth.java
@@ -12,9 +12,11 @@ public class TestAuth {
     ConfigPath configPath = new ConfigPath("dev");
     @Test
     public void TC_O1_givenInvalidToken_whenJoinBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        int brokerId = 1;
+        String tokenNull =null;
         verifyPut(
-                configPath.JOIN_GROUP_BROKER + 1,
-                "null",
+                configPath.JOIN_GROUP_BROKER + brokerId,
+                tokenNull,
                 new HashMap<String, Object>() {{
                     put("favorite", "true");
                 }},
@@ -25,9 +27,11 @@ public class TestAuth {
 
     @Test
     public void TC_O2_givenExpiredToken_whenJoinBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        int brokerId = 1;
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
         verifyPut(
-                configPath.JOIN_GROUP_BROKER + 1,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.JOIN_GROUP_BROKER + brokerId,
+                tokenExpired,
                 new HashMap<String, Object>() {{
                     put("favorite", "true");
                 }},
@@ -38,9 +42,10 @@ public class TestAuth {
 
     @Test
     public void TC_O3_givenExpiredToken_whenGetListBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
         verifyGet(
                 configPath.GET_LIST_BROKER ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                tokenExpired,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -48,9 +53,10 @@ public class TestAuth {
 
     @Test
     public void TC_O4_givenInvalidToken_whenGetListBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenNull =null;
         verifyGet(
                 configPath.GET_LIST_BROKER ,
-                "null," ,
+                tokenNull,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -58,9 +64,11 @@ public class TestAuth {
 
     @Test
     public void TC_O5_givenInvalidToken_whenGetDetailBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenNull =null;
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_DETAIL_BROKER + 1 ,
-                "null," ,
+                configPath.GET_DETAIL_BROKER + brokerId ,
+                tokenNull,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -68,9 +76,11 @@ public class TestAuth {
 
     @Test
     public void TC_O6_givenExpiredToken_whenGetDetailBroker_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_DETAIL_BROKER + 1 ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.GET_DETAIL_BROKER + brokerId ,
+                tokenExpired,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -78,9 +88,11 @@ public class TestAuth {
 
     @Test
     public void TC_O7_givenExpiredToken_whenGetDataProfitAndLoss_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_PROFIT_AND_LOSS_BROKER + 1  + "/deals" ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.GET_PROFIT_AND_LOSS_BROKER + brokerId  + "/deals" ,
+                tokenExpired,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -88,19 +100,32 @@ public class TestAuth {
 
     @Test
     public void TC_O8_givenInvalidToken_whenGetDataProfitAndLoss_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenNull =null;
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_PROFIT_AND_LOSS_BROKER + 1  + "/deals" ,
-                "null",
+                configPath.GET_PROFIT_AND_LOSS_BROKER + brokerId  + "/deals" ,
+                tokenNull,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
     }
 
+//    class BrokerAPI {
+//        public void BrokerAPI(String env) {
+//            this.env = env;
+//        }
+//
+//        public buildInvestmentPerformanceURL(brokerId) {
+//
+//        }
+//    }
     @Test
     public void TC_O9_givenInvalidToken_whenGetDataInvestmentPerformance_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenNull =null;
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_INVESTMENT_PERFORMANCE + 1  + "/investment-performance" ,
-                "null",
+                configPath.GET_INVESTMENT_PERFORMANCE + brokerId  + "/investment-performance" ,
+                tokenNull,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -108,9 +133,11 @@ public class TestAuth {
 
     @Test
     public void TC_10_givenExpiredToken_whenGetDataInvestmentPerformance_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_INVESTMENT_PERFORMANCE + 1  + "/investment-performance" ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.GET_INVESTMENT_PERFORMANCE + brokerId  + "/investment-performance" ,
+                tokenExpired,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
@@ -118,9 +145,11 @@ public class TestAuth {
 
     @Test
     public void TC_11_givenExpiredToken_whenGetDataInvestmentPerformance_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_INVESTMENT_PERFORMANCE + 1  + "/investment-performance" ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.GET_INVESTMENT_PERFORMANCE + brokerId  + "/investment-performance" ,
+                tokenExpired,
                 401,
                 "Vui lòng đăng nhập lại để tiếp tục sử dụng",
                 "UnauthorizedException");
@@ -128,9 +157,11 @@ public class TestAuth {
 
     @Test
     public void TC_12_givenExpiredToken_whenGetDataInvestEffects_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenExpired="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0";
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_INVEST_EFFECTS + 1  + "/backtest" ,
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3MSwiY3VzdF9pZCI6IjAwMDEwMDA0MDEiLCJzY29wZSI6IkxPR0lOIiwiaWF0IjoxNzA0OTQ1NjExLCJleHAiOjE3MDQ5NDkyMTF9.3vAsQ0USf1HNYU3Yavlzk7FOW97LmqkiYGiznc-CuR0",
+                configPath.GET_INVEST_EFFECTS + brokerId  + "/backtest" ,
+                tokenExpired,
                 401,
                 "Vui lòng đăng nhập lại để tiếp tục sử dụng",
                 "UnauthorizedException");
@@ -138,9 +169,11 @@ public class TestAuth {
 
     @Test
     public void TC_13_givenInvalidToken_whenGetDataInvestEffects_thenExpectedFailAndStatusCode401() throws Exception {
+        String tokenNull =null;
+        int brokerId = 1;
         verifyGet(
-                configPath.GET_INVEST_EFFECTS + 1  + "/backtest" ,
-                "null",
+                configPath.GET_INVEST_EFFECTS + brokerId  + "/backtest" ,
+                tokenNull,
                 401,
                 "Thông tin xác thực không hợp lệ",
                 "InvalidTokenException");
